d3198c049fe92e41491d829d7d26d9c54f9d0b18
rohith3695 <rohith.j.s42@gmail.com>
2026-04-16 19:19:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Herosection from "./components/herosection"
+
+function App() {
+  return (
+    <div>
+      <Herosection></Herosection>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/herosection.jsx
@@ -0,0 +1,9 @@
+const Herosection = () => {
+
+    return (
+        <h1>hi</h1>
+    )
+
+}
+
+export default Herosection;
